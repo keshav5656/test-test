d7da5987e727748918c6820786a53ad1130b1225
keshav5656 <117903968+keshav5656@users.noreply.github.com>
2022-11-30 21:16:21 +0530

    Create compare.py

---
+++ compare.py
@@ -0,0 +1,12 @@
+x = input()
+if x == start:
+  print('compare')
+  a = input('first number:')
+  b = input('second number')
+  if a > b:
+    print(a  + ">" + b)
+  elif b > a:
+    print(b  + ">" + a)
+  elif a == b:
+    print(a + "=" + b)
+print('--------------------------- please close ---------------------------')
